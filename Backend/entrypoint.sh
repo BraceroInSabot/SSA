ca8d6dfe127615d0f9310d50344372a1a8dd873e
Guilherme Bracero Gonzales <guibragon@gmail.com>
2026-03-18 15:42:42 -0300

    chore (DockerCompose): commands for container creation and sys configuration improved

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 
-echo "Aguardando o banco de dados (PostgreSQL) iniciar..."
-sleep 5 
+echo "Aguardando o banco de dados (PostgreSQL) iniciar corretamente..."
+python -c "
+import sys, time, os, psycopg2
+from psycopg2 import OperationalError
+while True:
+    try:
+        psycopg2.connect(
+            dbname=os.environ.get('DB_NAME'),
+            user=os.environ.get('DB_USER'),
+            password=os.environ.get('DB_PASSWORD'),
+            host=os.environ.get('DB_HOST', 'db'),
+            port=5432
+        )
+        break
+    except OperationalError:
+        print('Aguardando Postgres aceitar conexões...')
+        time.sleep(1)
+"
 
 echo "Aplicando migrações..."
 python manage.py migrate --noinput
 
-echo "Criando superusuário se não existir..."
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='$SYS_EMAIL').exists() or User.objects.create_superuser(email='$SYS_EMAIL', teacher_name='$SYS_NAME', password='$SYS_PASSWORD')"
+echo "Verificando/Criando superusuário..."
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='$SYS_EMAIL').exists() or User.objects.create_superuser(email='$SYS_EMAIL', name='$SYS_NAME', password='$SYS_PASSWORD')"
 
-echo "Iniciando Gunicorn..."
-exec gunicorn setup.wsgi:application --bind 0.0.0.0:8000
+echo "Iniciando Gunicorn com multiprocessamento..."
+exec gunicorn ssa.wsgi:application --bind 0.0.0.0:8000 -w 5
